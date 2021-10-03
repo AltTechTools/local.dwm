f2255261f36a5bce856416fa96da4cd942738bb3
fssSGJ <fsstweeter@gmail.com>
2021-10-03 17:36:18 +0200

    removed default bg file

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pulseaudio --start #you will never run any headphones ever again else
 #if no default is given, update Backgrounds List
-test -e defaultBG.conf || ./updatedefaultBackgrounds.sh
+test -e defaultBG.conf || "$HOME/.dwm/updatedefaultBackgrounds.sh"
 #test -e defaultBG.conf && ./setdefaultBackground.sh
 #exec svkbd-mobile-intl &
