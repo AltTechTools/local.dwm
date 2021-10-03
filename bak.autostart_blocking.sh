32588baf9392a31e0e11ef527895b4de23e1d9e0
fssSGJ <fsstweeter@gmail.com>
2021-10-03 16:48:05 +0200

    Deleted old moved files

--- bak.autostart_blocking.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pulseaudio --start #you will never run any headphones ever again else
-feh --bg-scale /home/damian/Bilder/Wallpapers/rwds.png
--- ttest.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-./updatebar.sh "time"
