57b526094668a14a85094e1d8e5aaf31b321b652
fssSGJ <fsstweeter@gmail.com>
2021-10-13 21:05:35 +0200

    test id

--- XrandRCtrl/test-rotate-normal.sh
+++ XrandRCtrl/test-rotate-normal.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 #xrandr --output "DSI-1" --rotate "left"
-./rotate-screen.sh -n "pointer:Goodix Capacitive TouchScreen"
+#./rotate-screen.sh -n "pointer:Goodix Capacitive TouchScreen"
+
+./rotate-screen.sh -n "8"
