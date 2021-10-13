e517b4f4a1cfdf4e0f0c8d4a8db88104087f9724
fssSGJ <fsstweeter@gmail.com>
2021-10-13 22:05:55 +0200

    new hot script to test

--- XrandRCtrl/hot-rotate-normal.sh
+++ XrandRCtrl/hot-rotate-normal.sh
@@ -5,5 +5,5 @@
 #./rotate-screen.sh -n "8"
 transformationmatrix=$(./rotate-screen-matrix.sh -n "8") || echo "failed"
 #echo "New: $transformationmatrix"
-#xinput set-prop "8" 'Coordinate Transformation Matrix' "$transformationmatrix"
-#xrandr --output "DSI-1" --transform "$transformationmatrix"
+xinput set-prop "8" 'Coordinate Transformation Matrix' "$transformationmatrix"
+xrandr --output "DSI-1" --transform "$transformationmatrix"
--- XrandRCtrl/hot-rotate.sh
+++ XrandRCtrl/hot-rotate.sh
@@ -5,6 +5,6 @@
 #./rotate-screen.sh -n "8"
 transformationmatrix=$(./rotate-screen-matrix.sh -- "8") || echo "failed"
 #echo "New: $transformationmatrix"
-#xinput set-prop "8" 'Coordinate Transformation Matrix' "$transformationmatrix"
-#xrandr --output "DSI-1" --transform "$transformationmatrix"
+xinput set-prop "8" 'Coordinate Transformation Matrix' "$transformationmatrix"
+xrandr --output "DSI-1" --transform "$transformationmatrix"
 
