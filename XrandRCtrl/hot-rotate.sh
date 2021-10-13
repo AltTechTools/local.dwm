2140723987152a91a06da2a5cef02e895b01759b
fssSGJ <fsstweeter@gmail.com>
2021-10-13 22:25:57 +0200

    update...

--- XrandRCtrl/hot-rotate.sh
+++ XrandRCtrl/hot-rotate.sh
@@ -4,7 +4,7 @@
 
 #./rotate-screen.sh -n "8"
 transformationmatrix=$(./rotate-screen-matrix.sh -- "8") || echo "failed"
+transformationmatrix2=$(echo "$transformationmatrix" | sed 's/ /,/g')
 #echo "New: $transformationmatrix"
-xinput set-prop "8" 'Coordinate Transformation Matrix' "$transformationmatrix"
-xrandr --output "DSI-1" --transform $transformationmatrix
-
+xinput set-prop "8" 'Coordinate Transformation Matrix' $transformationmatrix
+xrandr --output "DSI-1" --transform $transformationmatrix2
