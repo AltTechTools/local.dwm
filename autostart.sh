0f7332316f9493987aad007d0808680bc501508f
fssSGJ <fsstweeter@gmail.com>
2021-10-21 22:04:49 +0200

    Updated autostart for new default BG link

--- autostart.sh
+++ autostart.sh
@@ -9,13 +9,15 @@ sleep 0.2
 #Files(picntures) from Backgrounds are listed to select as a Background from the Menu (link files into this folder to have available as an option e.g.: ln -s <source filepath> <desired link name(will be shown as)>)
 # Files in the Folder can be real or symbolic Links
 #ls "Backgrounds/" | awk '{print $1";"$1"."$2}' FS="." > menus/autogen_ListBackground.conf
+touch "test.touch"
 ./updateBGMenuList.sh
-test -e defaultBG.conf && ./setdefaultBackground.sh
+test -e defaultBG.ln && ./setdefaultBackground.sh
 
+systemctl start sendmail.service &
 
 while true; do
         #dttm=$(date +"%a %R")
 	#xsetroot -name "$dttm"
-	#./settime.sh #e.v. error source
+	./settime.sh #e.v. error source
 	sleep 30
 done &
