94dc4597857dc1bb22c4c94d03e01ee9cd9e181f
pinephone <fsstweeter@gmail.com>
2021-11-07 16:08:40 +0100

    changed autostart, to always update the list of available backgrounds

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -2,7 +2,8 @@
 #pulseaudio --start #you will never run any headphones ever again else
 #if no default is given, update Backgrounds List
 cd "$HOME/.dwm/"
-test -e defaultBG.ln || ./updatedefaultBackgrounds.sh
+#test -e defaultBG.ln || 
+./updatedefaultBackgrounds.sh
 #test -e defaultBG.conf && ./setdefaultBackground.sh
 #exec svkbd-mobile-intl &
 pulseaudio --start
